31eb39a0804ec836af09069d1544fa56a0457041
studentsimone <55529034+studentsimone@users.noreply.github.com>
2019-10-03 14:36:52 +0200

    Initial Commit

---
+++ OpenWeatherSln/OpenWeather/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  Console.WriteLine("Hello World!");
+        }
+    } 
+}
